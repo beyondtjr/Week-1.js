e5398f9b17bd5397e3285a4101dc34372410f38d
beyondtjr <43766714+beyondtjr@users.noreply.github.com>
2018-10-02 12:30:36 -0700

    Update Day 2.js

--- Day 2.js
+++ Day 2.js
@@ -11,10 +11,11 @@ function message(product, inBudget){
 var product = { name: "OledMonitor", price: 300.00 }
 var budget = 275.00
 // Write a function checkBudget that takes a product, budget and the message function as arguments, determines whether the product is within budget and returns the result of the message function.
-function checkBudget(product, budget, message()){
+function checkBudget(product, budget, message){
     if (product.price <= budget) {
       return message(product,true)
     } else {
       return message(product,false)
     }
 }
+checkBudget("OledMonitor", budget, message);
